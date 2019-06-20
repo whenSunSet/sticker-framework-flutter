3479370551bc301d0262f1200947a2a7bdcd21e7
WhenSunset <xuekelindun@kuaishou.com>
2019-06-20 12:13:58 +0800

    test

--- lib/libsample/test_main.dart
+++ lib/libsample/test_main.dart
@@ -1,57 +1,117 @@
 import 'package:flutter/material.dart';
-import 'package:sticker/libsample/test_sticker_element.dart';
-import 'package:sticker_framework/element_container_widget.dart';
-import 'package:sticker_framework/rule_line_element_container_widget.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 // 用于测试已经发布的包
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    RuleLineElementContainerState decorationElementContainerWidgetState = RuleLineElementContainerState();
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Stack(
-        alignment: AlignmentDirectional.topStart,
-        children: <Widget>[
-          DecoratedBox(
-            decoration: BoxDecoration(
-                color: Colors.grey
-            ),
-            child: ElementContainerWidget(
-                decorationElementContainerWidgetState),
-          ),
-          Positioned(
-            child: RaisedButton(
-              child: Text("add"),
-              onPressed: () {
-                StickerElement stickerElement = StickerElement(100, 100);
-                decorationElementContainerWidgetState
-                    .addSelectAndUpdateElement(
-                    stickerElement);
-              },
-            ),
-            left: 0,
-            top: 50,
-          ),
-          Positioned(
-            child: RaisedButton(
-              child: Text("delete"),
-              onPressed: () {
-                decorationElementContainerWidgetState
-                    .unSelectDeleteAndUpdateTopElement();
-              },
-            ),
-            left: 100,
-            top: 50,
-          ),
-        ],
-      ),
-    );
+    return AW();
   }
 }
 
+class AW extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AWS();
+  }
+}
+
+class AWS extends State<AW> {
+  final TextEditingController _controller = new TextEditingController();
+  bool a = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(milliseconds: 3000), () {
+      setState(() {
+        print("state");
+        a = !a;
+        WidgetsBinding.instance.addPostFrameCallback(onAfterRendering);
+      });
+    });
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        resizeToAvoidBottomPadding: true,
+        appBar: AppBar(
+          title: Text('输入和选择'),
+        ),
+        body: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: double.infinity,
+            minWidth: double.infinity,
+          ),
+          child: get(),
+        ),
+      ),
+    );
+  }
+
+  Widget get() {
+//    TextField textField = TextField(
+//      controller: _controller,
+//      decoration: InputDecoration(
+//        contentPadding: EdgeInsets.all(100.0),
+//      ),
+//    );
+//    if (a) {
+//      return Stack(
+//        children: <Widget>[
+//          textField,
+//        ],
+//      );
+//    } else {
+//
+//    }
+    return GestureDetector(
+      child: Stack(
+//          children: <Widget>[
+//            textField,
+////          Text('输入和选择'),
+//          ],
+        children: <Widget>[
+          Text("")
+        ],
+      ),
+      onLongPress: longs,
+      onTap: taps,
+      behavior: HitTestBehavior.opaque,
+    );
+  }
+
+  void longs() {
+    print("longs");
+  }
+
+  void taps() {
+    print("taps");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    WidgetsBinding.instance.addPostFrameCallback(onAfterRendering);
+  }
+
+  @override
+  void didUpdateWidget(Widget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    WidgetsBinding.instance.addPostFrameCallback(onAfterRendering);
+  }
+
+  void onAfterRendering(Duration timeStamp) {
+    RenderObject renderObject = context?.findRenderObject();
+    if (renderObject != null) {
+      Size size = renderObject.paintBounds.size;
+      Vector3 vector3 = renderObject.getTransformTo(null)?.getTranslation();
+      print("x:${vector3.x}, y:${vector3.y}, width:${size.width}, height:${size
+          .height}");
+    }
+  }
+
+}
